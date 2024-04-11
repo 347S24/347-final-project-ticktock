a44f541ab8424e0b14d42a88844d6f5b8a16a59e
Rue927 <88685257+Rue927@users.noreply.github.com>
2024-04-11 12:06:16 -0400

    Why gap :'(

--- ticktock/frontend/src/App.jsx
+++ ticktock/frontend/src/App.jsx
@@ -12,35 +12,8 @@ function App() {
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.djangoproject.com/" target="_blank">
-          <img src={djangoLogo} className="logo" alt="Django logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Django + Aarushi</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p>
-          You are logged in as {username}.
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */
-
-        <div>
-          <title>Home page for Ticktock</title>
+      {
+        <div className="container">
           <header>Ticktock</header>
 
           <main>
@@ -48,8 +21,7 @@ function App() {
             <p>Here is where a tracker would technically be.</p>
           </main>
 
-          <foot>Ticktock Inc.</foot>
-
+          <footer>Ticktock Inc.</footer>
         </div>
 
 
